9a42d40a25ad80b09245c7373cc7554ecd9c711d
atharvyadav96k <atharvyadav96k@gmail.com>
2026-09-22 23:33:01 +0530

    Add GameObject and Transform classes for game object management and positioning

---
+++ objects/game_object.py
@@ -0,0 +1,11 @@
+
+class GameObject():
+    def __init__(self, id):
+        self.id = id
+        
+    def update(self):
+        pass
+
+    def render(self):
+        pass
+
---
+++ objects/transform.py
@@ -0,0 +1,8 @@
+
+class Transform():
+    def __init__(self, position, size):
+        self.position = position
+        self.size = size
+
+    def move(self, x, y):
+        self.position (x, y)
