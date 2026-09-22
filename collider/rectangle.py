29fab08cadb30d7fe52f672d45dad400b7be051c
atharvyadav96k <atharvyadav96k@gmail.com>
2026-09-22 23:42:58 +0530

    Add RectangleCollider class for collision detection using pygame

---
+++ collider/rectangle.py
@@ -0,0 +1,12 @@
+
+import pygame
+
+class RectangleCollider():
+    def __init__(self, game_object):
+        self.game_object = game_object
+
+    def get_rect(self):
+        return pygame.Rect(
+            self.game_object.transform.position,
+            self.game_object.transform.size
+        )
